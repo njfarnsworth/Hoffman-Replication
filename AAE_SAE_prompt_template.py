18cbcccb2e3bcbabc3adb9ecd99c9027b4e51c8a
mrmorel15 <148816764+mrmorel15@users.noreply.github.com>
2025-10-06 21:05:42 -0400

    Fixed silly mistakes in AAE_SAE_prompt_template.py

--- AAE_SAE_prompt_template.py
+++ AAE_SAE_prompt_template.py
@@ -1,8 +1,11 @@
-make_data(filename: str) -> list:
+import csv
+import pandas as pd
+
+def make_data(filename: str) -> list:
   # with open('sae_aave_samples-checkpoint.csv', 'r', newline='') as file:
   #   reader = csv.reader(file)
   #   for row in reader:
-  df = pd.read_csv('sae_aave_samples-checkpoint.csv')
+  df = pd.read_csv(filename)
   data = []
   data_row = ['sentid', 'pairid', 'type', 'comparison']
   data.append(data_row)
@@ -20,9 +23,14 @@ make_data(filename: str) -> list:
       pairid = row
       data_row = [sentid, pairid, type, comparison]
       data.append(data_row)
-return data
+  return data
 
-write_csv(data: list):
+def write_csv(data: list):
   with open('AAE_SAE_MP.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(data)
+
+def main():
+  filename = 'sae_aave_samples-checkpoint.csv'
+  data = make_data(filename)
+  write_csv(data)

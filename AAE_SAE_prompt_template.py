0a27a4ead5ae0d7ba3e1808fcaf6a9a88a415baa
mrmorel15 <148816764+mrmorel15@users.noreply.github.com>
2025-10-06 21:44:01 -0400

    Final AAE_SAE_prompt_template.py

--- AAE_SAE_prompt_template.py
+++ AAE_SAE_prompt_template.py
@@ -2,22 +2,20 @@ import csv
 import pandas as pd
 
 def make_data(filename: str) -> list:
-  # with open('sae_aave_samples-checkpoint.csv', 'r', newline='') as file:
-  #   reader = csv.reader(file)
-  #   for row in reader:
+
   df = pd.read_csv(filename)
   data = []
   data_row = ['sentid', 'pairid', 'type', 'comparison', 'sentence']
   data.append(data_row)
-  #data_row = []
+
   for i, row in df.iterrows():
     sae_sentence = row['SAE']
     aae_sentence = row['AAVE']
-    #if sae_sentence:
-    data_row = [i, row, "SAE", "expected", sae_sentence]
+
+    data_row = [i, i, "SAE", "expected", sae_sentence]
     data.append(data_row)
-    #else:
-    data_row = [i + 1, row, "AAE", "unexpected", aae_sentence]
+
+    data_row = [i + 1, i, "AAE", "unexpected", aae_sentence]
     data.append(data_row)
   return data
 
